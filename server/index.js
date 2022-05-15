1cfd467e2d2e1c2c52c4b4542f5b426158d63501
Tomas Sanchez <tms90@outlook.es>
2022-05-15 13:10:13 +0200

    Added middlewares file with error.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const { notFoundError, generalError } = require("./middlewares/error");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(generalError);
+app.use(notFoundError);
 
 module.exports = app;
