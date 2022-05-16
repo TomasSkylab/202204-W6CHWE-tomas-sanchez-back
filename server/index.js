52e71abe938d61043ec56e5e75c68a14e3c4f82e
Tomas Sanchez <tms90@outlook.es>
2022-05-16 02:22:33 +0200

    Installed cors dependence and use a variable in routers/index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 app.use("/", robotsRouter);
 app.use(generalError);
 app.use(notFoundError);
